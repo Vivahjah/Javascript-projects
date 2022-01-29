32ca6e0ddbb6867a2523e0cecf5a1593e2a7a0f9
Vivahjah <mengwa44@yahoo.com>
2022-01-29 03:15:38 +0100

    completed the app with Local storage

--- Notes-app/app.js
+++ Notes-app/app.js
@@ -1,20 +1,73 @@
-const getE = (element) => document.querySelector(element);
+const addBtn = document.getElementById('add');
 
-const notesEl = getE('.notes');
-const editBtn = getE('.edit');
-const deleteBtn = getE('.delete');
+const notes = JSON.parse(localStorage.getItem('notes'));
 
-const main = notesEl.querySelector('.main');
-const textArea = notesEl.querySelector('textarea');
 
-editBtn.addEventListener('click', () => {
 
-    main.classList.toggle('hidden');
-    textArea.classList.toggle('hidden');
+addBtn.addEventListener('click', () => {
+    addNewNote();
+
 });
 
-textArea.addEventListener("input", (e) => {
-    const { value } = e.target;
+const addNewNote = (text = '') => {
+        const note = document.createElement('div');
+        const getE = (element) => note.querySelector(element);
+        note.classList.add('note');
 
-    main.innerHTML = marked.parse(value);
-});
+        note.innerHTML = String.raw `
+        <div class = "notes">
+            <div class = "tools">
+                <button class="edit"><i class = "fas fa-edit"></i></button>
+                <button class="delete"> <i class = "fas fa-trash-alt"></i></button>
+            </div>
+            <div class="main"></div>
+            <textarea></textarea>
+       </div>
+    
+            `;
+
+        const editBtn = getE('.edit');
+        const deleteBtn = getE('.delete');
+
+        const main = note.querySelector('.main');
+        const textArea = note.querySelector('textarea');
+
+        textArea.value = text;
+        main.innerHTML = marked.parse(text);
+
+        editBtn.addEventListener('click', () => {
+
+            main.classList.toggle('hidden');
+            textArea.classList.toggle('hidden');
+        });
+
+        deleteBtn.addEventListener('click', () => {
+            note.remove();
+
+            updateLS();
+
+        });
+
+        textArea.addEventListener("input", (e) => {
+            const { value } = e.target;
+
+            main.innerHTML = marked.parse(value);
+
+            updateLS();
+        });
+
+        document.body.appendChild(note);
+    }
+    // Adding to Local storage
+const updateLS = () => {
+    const noteText = document.querySelectorAll('textarea');
+    const notes = [];
+
+    noteText.forEach(note => notes.push(note.value));
+    localStorage.setItem('notes', JSON.stringify(notes));
+}
+if (notes) {
+    notes.forEach(note => {
+        addNewNote(note);
+    });
+}
